da0eadb9042e5780ca8fac292c697f64fb8e7a86
Maryem Souayah <maryemsouayah3@gmail.com>
2020-03-13 01:06:53 +0100

    Updates

---
+++ precache-manifest.7f68ce5b8369e3f0da7436eeee0a4f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbf3d43eb3e3fa2272a14dc416902f6",
+    "url": "/Tp3/index.html"
+  },
+  {
+    "revision": "a851fd5ecdd74ca05d67",
+    "url": "/Tp3/static/css/main.9e55038b.chunk.css"
+  },
+  {
+    "revision": "7f85d907ae32c538c419",
+    "url": "/Tp3/static/js/2.6866ce9a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Tp3/static/js/2.6866ce9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a851fd5ecdd74ca05d67",
+    "url": "/Tp3/static/js/main.be7fdb08.chunk.js"
+  },
+  {
+    "revision": "285cb16f1edb01f40a8e",
+    "url": "/Tp3/static/js/runtime-main.56342623.js"
+  }
+]);
